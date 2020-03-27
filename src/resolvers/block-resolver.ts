93b507200b0fb3a61298633671ba92ea6f71637d
Andrew Paulicek <andrew.paulicek@agile-ventures.com>
2020-03-27 19:55:03 +0100

    block-resolver refactored

--- src/resolvers/block-resolver.ts
+++ src/resolvers/block-resolver.ts
@@ -1,51 +1,20 @@
 import { BlockResponse, OperationEntry, OpKind } from '@taquito/rpc';
 import { cloneDeep } from 'lodash';
 
-var filterOperations = (operations: OperationEntry[], opKind: OpKind) => {
-    let filteredOperations = cloneDeep(operations.filter(r => r.contents.some(op => op.kind == opKind)));
-    filteredOperations = filteredOperations.map(o => {
-        o.contents = o.contents.filter(c => c.kind == opKind)
-        return o;
-    });
-    return filteredOperations;
-}
-
 export const blockResolver = {
     Block: {
-        activations(root: BlockResponse) {
-            return filterOperations(root.operations[2], OpKind.ACTIVATION)
-        },
-        ballots(root: BlockResponse) {
-            return filterOperations(root.operations[1], OpKind.BALLOT)
-        },
-        delegations(root: BlockResponse) {
-            return filterOperations(root.operations[3], OpKind.DELEGATION)
-        },
-        double_baking_evidence(root: BlockResponse) {
-            return filterOperations(root.operations[2], OpKind.DOUBLE_BAKING_EVIDENCE)
-        },
-        double_endorsement_evidence(root: BlockResponse) {
-            return filterOperations(root.operations[2], OpKind.DOUBLE_ENDORSEMENT_EVIDENCE)
-        },
-        endorsements(root: BlockResponse) {
-            return filterOperations(root.operations[0], OpKind.ENDORSEMENT)
-        },
-        originations(root: BlockResponse) {
-            return filterOperations(root.operations[3], OpKind.ORIGINATION)
-        },
-        proposals(root: BlockResponse) {
-            return filterOperations(root.operations[1], OpKind.PROPOSALS)
-        },
-        reveals(root: BlockResponse) {
-            return filterOperations(root.operations[3], OpKind.REVEAL);
-        },
-        seed_nonce_revelations(root: BlockResponse) {
-            return filterOperations(root.operations[1], OpKind.SEED_NONCE_REVELATION)
-        },
-        transactions(root: BlockResponse) {
-            return filterOperations(root.operations[3], OpKind.TRANSACTION)
-        },
-        async operations(root: BlockResponse, args: { kind: OpKind }) {
+        activations: async (root: BlockResponse) => filterOperations(root.operations[2], OpKind.ACTIVATION),
+        ballots: async (root: BlockResponse) => filterOperations(root.operations[1], OpKind.BALLOT),
+        delegations: async (root: BlockResponse) => filterOperations(root.operations[3], OpKind.DELEGATION),
+        double_baking_evidence: async (root: BlockResponse) => filterOperations(root.operations[2], OpKind.DOUBLE_BAKING_EVIDENCE),
+        double_endorsement_evidence: async (root: BlockResponse) => filterOperations(root.operations[2], OpKind.DOUBLE_ENDORSEMENT_EVIDENCE),
+        endorsements: async (root: BlockResponse) => filterOperations(root.operations[0], OpKind.ENDORSEMENT),
+        originations: async (root: BlockResponse) => filterOperations(root.operations[3], OpKind.ORIGINATION),
+        proposals: async (root: BlockResponse) => filterOperations(root.operations[1], OpKind.PROPOSALS),
+        reveals: async (root: BlockResponse) => filterOperations(root.operations[3], OpKind.REVEAL),
+        seed_nonce_revelations: async (root: BlockResponse) => filterOperations(root.operations[1], OpKind.SEED_NONCE_REVELATION),
+        transactions: async (root: BlockResponse) => filterOperations(root.operations[3], OpKind.TRANSACTION),
+        operations: async (root: BlockResponse, args: { kind: OpKind }) => {
             if (args && args.kind) {
                 let filtered = root.operations.map(ops => filterOperations(ops, args.kind));
                 return filtered;
@@ -54,3 +23,12 @@ export const blockResolver = {
         }
     }
 } 
+
+async function filterOperations(operations: OperationEntry[], opKind: OpKind) {
+    let filteredOperations = cloneDeep(operations.filter(r => r.contents.some(op => op.kind == opKind)));
+    filteredOperations = filteredOperations.map(o => {
+        o.contents = o.contents.filter(c => c.kind == opKind)
+        return o;
+    });
+    return filteredOperations;
+}
