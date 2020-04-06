67775eccead5b76c146d1d5b6ea43d745a28deea
mbodecek <mbodecek@gmail.com>
2020-04-06 21:09:41 +0200

    support limiting by maximum count in block query

--- src/resolvers/queries/blocks/blocks-query-resolver.ts
+++ src/resolvers/queries/blocks/blocks-query-resolver.ts
@@ -1,6 +1,5 @@
 import { BlockResponse } from '@taquito/rpc';
 import { UserInputError } from 'apollo-server-express';
-import { reverse } from 'lodash';
 import { container } from 'tsyringe';
 
 import { TezosRpcService } from '../../../services/tezos-rpc-service';
@@ -8,45 +7,68 @@ import { Block } from '../../../types/types';
 import { convertResponse } from './block-utils';
 
 const tezosRpcService = container.resolve(TezosRpcService);
+
+function fetchBlock(block: string): Promise<BlockResponse> {
+    return tezosRpcService.client.getBlock({ block });
+}
+
+// let's save some calls by saving the fetched blocks
+async function getLevel(argument: string | null): Promise<[number, BlockResponse | null]> {
+    if (argument == null) {
+        var block = await fetchBlock('head');
+        return [block.metadata.level.level, block];
+    }
+
+    let parsed = Number(argument);
+    if (isNaN(parsed)) {
+        // try fetching hash or head
+        var block = await fetchBlock(argument);
+        return [block.metadata.level.level, block];
+    } else if (parsed < 0) {
+        // relative to head
+        var block = await fetchBlock('head');
+        return [block.metadata.level.level + parsed, null];
+    }
+    return [parsed, await fetchBlock(argument)];
+}
+
 export const blocksQueryResolver = {
     Query: {
-        async blocks(obj: any, args: { from: string; to: string | null }, context: any): Promise<Block[]> {
-            function fetchBlock(block: string): Promise<BlockResponse> {
-                return tezosRpcService.client.getBlock({ block });
+        async blocks(obj: any, args: { from: string | null; to: string | null, count: number | null }, context: any): Promise<Block[]> {
+            if (args.from == null && args.to == null && args.count == null) {
+                throw new UserInputError(`Neither "from", "to" or "count" argument specified`);
+            }
+            if (args.from != null && args.to != null && args.count != null) {
+                throw new UserInputError(`You cannot limit your query from both ends ("from" and "to") and by maximum count at the same time`);
+            }
+            if (args.count != null && args.count <= 0) {
+                throw new UserInputError(`The "count" argument has to be greater than 0`);
             }
 
-            async function getLevel(argument: string | null): Promise<[number, BlockResponse]> {
-                if (argument == null) {
-                    var block = await fetchBlock('head');
-                    return [block.metadata.level.level, block];
+            let fromLevel: number, toLevel: number;
+            let firstBlock: BlockResponse | null, lastBlock: BlockResponse | null;
+            if (args.count == null) {
+                [fromLevel, firstBlock] = await getLevel(args.from);
+                [toLevel, lastBlock] = await getLevel(args.to);
+            } else {
+                if (args.from != null) {
+                    [fromLevel, firstBlock] = await getLevel(args.from);
+                    [toLevel, lastBlock] = await getLevel((fromLevel + args.count - 1).toString());
+                } else {
+                    [toLevel, lastBlock] = await getLevel(args.to);
+                    [fromLevel, firstBlock] = await getLevel((toLevel - args.count + 1).toString());
                 }
-
-                let parsed = Number(argument);
-                if (isNaN(parsed)) {
-                    // try fetching hash or head
-                    var block = await fetchBlock(argument);
-                    return [block.metadata.level.level, block];
-                } else if (parsed < 0) {
-                    // relative to head
-                    var block = await fetchBlock('head');
-                    return [block.metadata.level.level + parsed, block];
-                }
-                return [parsed, await fetchBlock(argument)];
             }
-
-            // let's save some calls by fetching the whole first and last blocks
-            let [fromLevel, firstBlock] = await getLevel(args.from);
-            let [toLevel, lastBlock] = await getLevel(args.to);
 
             if (toLevel - fromLevel + 1 > +process.env.MAX_BLOCKS!) {
                 throw new UserInputError(`Number of blocks has to be lower than ${process.env.MAX_BLOCKS!}.`);
             }
 
-            let blocks: Block[] = [convertResponse(firstBlock)];
+            let blocks: Block[] = [convertResponse(firstBlock || await fetchBlock(fromLevel.toString()))];
             for (let level = fromLevel + 1; level < toLevel; level++) {
                 blocks.push(convertResponse(await tezosRpcService.client.getBlock({ block: level.toString() })));
             }
-            blocks.push(convertResponse(lastBlock));
+            blocks.push(convertResponse(lastBlock || await fetchBlock(toLevel.toString())));
             return blocks;
         },
     },
