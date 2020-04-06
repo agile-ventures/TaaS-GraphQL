7bb7773bda53952899badf6fe937f4deae2e204e
mbodecek <mbodecek@gmail.com>
2020-04-07 01:07:20 +0200

    fix a corner case when "from" is defined, but there is fewer blocks than "count" left

--- src/resolvers/queries/blocks/blocks-query-resolver.ts
+++ src/resolvers/queries/blocks/blocks-query-resolver.ts
@@ -45,31 +45,57 @@ export const blocksQueryResolver = {
                 throw new UserInputError(`The "count" argument has to be greater than 0`);
             }
 
-            let fromLevel: number, toLevel: number;
-            let firstBlock: BlockResponse | null, lastBlock: BlockResponse | null;
+            // calculate the boundaries and potentially keep some fetched blocks for later
+            let fromLevel: number;
+            let count: number;
+            let firstBlock: BlockResponse | null;
+            let lastBlock: BlockResponse | null;
             if (args.count == null) {
+                let toLevel: number;
                 [fromLevel, firstBlock] = await getLevel(args.from);
                 [toLevel, lastBlock] = await getLevel(args.to);
+                count = toLevel - fromLevel + 1;
             } else {
                 if (args.from != null) {
                     [fromLevel, firstBlock] = await getLevel(args.from);
-                    [toLevel, lastBlock] = await getLevel((fromLevel + args.count - 1).toString());
+                    lastBlock = null;
+                    count = args.count;
                 } else {
+                    let toLevel: number;
                     [toLevel, lastBlock] = await getLevel(args.to);
-                    [fromLevel, firstBlock] = await getLevel((toLevel - args.count + 1).toString());
+                    fromLevel = Math.max(0, toLevel - args.count + 1);
+                    firstBlock = null;
+                    count = toLevel - fromLevel + 1;
                 }
             }
 
-            if (toLevel - fromLevel + 1 > +process.env.MAX_BLOCKS!) {
+            // handle corner cases first
+            if (count <= 0) {
+                return [];
+            }
+            if (count > +process.env.MAX_BLOCKS!) {
                 throw new UserInputError(`Number of blocks has to be lower than ${process.env.MAX_BLOCKS!}.`);
             }
 
             let blocks: Block[] = [convertResponse(firstBlock || await fetchBlock(fromLevel.toString()))];
-            for (let level = fromLevel + 1; level < toLevel; level++) {
-                blocks.push(convertResponse(await fetchBlock(level.toString())));
-            }
-            if (fromLevel != toLevel) {
-                blocks.push(convertResponse(lastBlock || await fetchBlock(toLevel.toString())));
+            for (let i = 1; i < count; i++) {
+                if (i == count - 1 && lastBlock != null) {
+                    // if we are at the last block and we have fetched it already...
+                    blocks.push(convertResponse(lastBlock));
+                } else {
+                    // fetch the block
+                    let block: BlockResponse;
+                    try {
+                        block = await fetchBlock((fromLevel + i).toString());
+                    } catch (e) {
+                        if (e.status == 404) {
+                            // we are at the end of the list
+                            break;
+                        }
+                        throw e;
+                    }
+                    blocks.push(convertResponse(block));    
+                }
             }
             return blocks;
         },
