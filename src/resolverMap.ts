06c2e31b9ec3947eda0ff0eb44aedb95d2d2eace
Roxin Cernica <roxin.cernica@gmail.com>
2020-04-07 20:59:53 +0000

    - move delegate and contract under block
    - load entrypoint for contract

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 
 import { GraphQLDateTime } from 'graphql-iso-date';
 import { IResolvers } from 'graphql-tools';
+import GraphQLJSONObject from 'graphql-type-json';
 import { merge } from 'lodash';
 
 import { blockResolver } from './resolvers/block-resolver';
+import { contractResolver } from './resolvers/contract-resolver';
 import { enumResolver } from './resolvers/enum-resolver';
 import { internalOperationResultTransactionResolver } from './resolvers/internal-operation-result-info-resolvers';
 import { internalOperationResultResolver } from './resolvers/internal-operation-result-resolver';
@@ -12,16 +14,19 @@ import { michelsonV1ExpressionResolver } from './resolvers/michelson-v1-expressi
 import { operationContentResolver } from './resolvers/operation-content-resolver';
 import { blockQueryResolver } from './resolvers/queries/blocks/block-query-resolver';
 import { blocksQueryResolver } from './resolvers/queries/blocks/blocks-query-resolver';
-import { delegatesQueryResolver } from './resolvers/queries/delegates/delegates-query-resolver';
 import { operationResolver } from './resolvers/operation-resolver';
 
 const dateTimeResolver: IResolvers = {
     DateTime: GraphQLDateTime,
 };
 
-const queries = merge(blockQueryResolver, blocksQueryResolver, delegatesQueryResolver);
-const typeResolvers = merge(dateTimeResolver, operationContentResolver, michelsonV1ExpressionResolver, internalOperationResultResolver, enumResolver);
-const schemaResolvers = merge(internalOperationResultTransactionResolver, blockResolver, operationResolver);
+const jsonResolver: IResolvers = {
+    JSON: GraphQLJSONObject,
+};
+
+const queries = merge(blockQueryResolver, blocksQueryResolver);
+const typeResolvers = merge(dateTimeResolver, jsonResolver, operationContentResolver, michelsonV1ExpressionResolver, internalOperationResultResolver, enumResolver);
+const schemaResolvers = merge(internalOperationResultTransactionResolver, blockResolver, contractResolver, operationResolver);
 const resolvers = merge(queries, typeResolvers, schemaResolvers);
 
 export default resolvers;
--- src/server.ts
+++ src/server.ts
@@ -14,31 +14,32 @@ dotenv.config();
 
 const app = express();
 const config: ApolloServerExpressConfig = {
-  schema,
-  validationRules: [depthLimit(7)],
-}
+    schema,
+    validationRules: [depthLimit(7)],
+};
 
 if (process.env.GRAPHQL_ENABLE_PLAYGROUND === 'true') {
-  config.playground = true;
+    config.playground = true;
 }
 
 if (process.env.GRAPHQL_ENABLE_INTROSPECTION === 'true') {
-  config.introspection = true;
+    config.introspection = true;
 }
 
 if (process.env.ENABLE_API_KEY === 'true' && process.env.API_KEY) {
-  config.context = (ctx: ExpressContext) => {
-    if (ctx.req.header('X-TaaS-Key') !== process.env.API_KEY) {
-      throw new AuthenticationError('Provided X-TaaS-Key header value is wrong or empty!');
-    }
-  }
+    config.context = (ctx: ExpressContext) => {
+        if (ctx.req.header('X-TaaS-Key') !== process.env.API_KEY) {
+            throw new AuthenticationError('Provided X-TaaS-Key header value is wrong or empty!');
+        }
+    };
 }
 
 const server = new ApolloServer(config);
 app.use('*', cors());
 app.use(compression());
 server.applyMiddleware({ app, path: '/graphql' });
+
 const httpServer = createServer(app);
-httpServer.listen(
-  { host: process.env.HOST, port: process.env.PORT },
-  (): void => console.log(`\n🚀      GraphQL is now running on http://${process.env.HOST}:${process.env.PORT}/graphql`));
+httpServer.listen({ host: process.env.HOST, port: process.env.PORT }, (): void =>
+    console.log(`\n🚀      GraphQL is now running on http://${process.env.HOST}:${process.env.PORT}/graphql`)
+);
