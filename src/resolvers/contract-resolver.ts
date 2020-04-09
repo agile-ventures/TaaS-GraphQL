1d5b34752d637501a3e943cc7c2e790f66bef7f8
Miroslav Bodecek <mbodecek@gmail.com>
2020-04-09 17:07:39 +0200

    fix storage and endpoints missing in older protocols

--- src/resolvers/contract-resolver.ts
+++ src/resolvers/contract-resolver.ts
@@ -2,52 +2,50 @@ import { container } from 'tsyringe';
 
 import { TezosRpcService } from '../services/tezos-rpc-service';
 import { ContractEntrypoint, ContractResponse, EntrypointPath, ManagerKeyResponse, StorageResponse, DelegateResponse } from '../types/types';
-import { ManagerKeyResponse as TaquitoManagerKeyResponse } from '@taquito/rpc';
+import { ManagerKeyResponse as TaquitoManagerKeyResponse, EntrypointsResponse } from '@taquito/rpc';
 
 const tezosRpcService = container.resolve(TezosRpcService);
 
+async function handleNotFound<T>(run: () => Promise<T>): Promise<T | null> {
+    try {
+        return await run();
+    } catch (e) {
+        if (e.status === 404) {
+            return null;
+        }
+        throw e;
+    }
+}
+
 export const contractResolver = {
     Contract: {
-        async entrypoint(contract: ContractResponse): Promise<ContractEntrypoint> {
-            const result = await tezosRpcService.client.getEntrypoints(contract.address, { block: contract.blockHash });
-            return {
-                ...result,
-                unreachable: result.unreachable
-                    ? {
-                          ...result.unreachable,
-                          path: result.unreachable.path.map(p => EntrypointPath[p]), //is there a better way to go from ('left' | 'right') to Enum?
-                      }
-                    : undefined,
-            };
+        async entrypoint(contract: ContractResponse): Promise<ContractEntrypoint | null> {
+            const result = await handleNotFound(() => tezosRpcService.client.getEntrypoints(contract.address, { block: contract.blockHash }));
+            if (result != null) {
+                return {
+                    ...result,
+                    unreachable: result.unreachable
+                        ? {
+                              ...result.unreachable,
+                              path: result.unreachable.path.map(p => EntrypointPath[p]), //is there a better way to go from ('left' | 'right') to Enum?
+                          }
+                        : undefined,
+                };
+            }
+            return null;
         },
         async manager_key(contract: ContractResponse): Promise<ManagerKeyResponse | null> {
-            // apparently can return a 404
-            try {
-                const result = await tezosRpcService.client.getManagerKey(contract.address, { block: contract.blockHash });
+            const result = await handleNotFound(() => tezosRpcService.client.getManagerKey(contract.address, { block: contract.blockHash }));
+            if (result != null) {
                 return managerKeyIsString(result) ? { key: result } : { key: result.key, invalid: true };
-            } catch (e) {
-                if (e.status === 404) {
-                    return null;
-                }
-
-                throw e;
             }
+            return null;
         },
-        async storage(contract: ContractResponse): Promise<StorageResponse> {
-            const result = await tezosRpcService.client.getStorage(contract.address, { block: contract.blockHash });
-            return result;
+        storage(contract: ContractResponse): Promise<StorageResponse | null> {
+            return handleNotFound(() => tezosRpcService.client.getStorage(contract.address, { block: contract.blockHash }));
         },
-        async delegate(contract: ContractResponse): Promise<DelegateResponse> {
-            try {
-                const result = await tezosRpcService.client.getDelegate(contract.address, { block: contract.blockHash });
-                return result;
-            } catch (e) {
-                if (e.status === 404) {
-                    return null;
-                }
-
-                throw e;
-            }
+        delegate(contract: ContractResponse): Promise<DelegateResponse> {
+            return handleNotFound(() => tezosRpcService.client.getDelegate(contract.address, { block: contract.blockHash }));
         },
     },
 };
