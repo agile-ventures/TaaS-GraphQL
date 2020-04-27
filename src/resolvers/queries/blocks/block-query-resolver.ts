5af7659b2a7c69d1243a3bfd592428b1808b4c9a
Miroslav Bodecek <mbodecek@gmail.com>
2020-04-27 11:47:41 +0000

    convert to camelCase

--- src/resolvers/queries/blocks/block-query-resolver.ts
+++ src/resolvers/queries/blocks/block-query-resolver.ts
@@ -1,6 +1,6 @@
 import { TezosService } from '../../../services/tezos-service';
 import { container } from 'tsyringe';
-import { convertResponse as responseToBlock } from './block-utils';
+import { convertResponseOrNull } from '../../utils';
 import { Block } from '../../../types/types';
 
 const tezosRpcService = container.resolve(TezosService);
@@ -8,7 +8,7 @@ const tezosRpcService = container.resolve(TezosService);
 export const blockQueryResolver = {
     Query: {
         async block(obj: any, args: { block: string | null }): Promise<Block | null> {
-            return responseToBlock(await TezosService.handleNotFound(() => tezosRpcService.client.getBlock({ block: args.block || 'head' })));
+            return convertResponseOrNull(await TezosService.handleNotFound(() => tezosRpcService.client.getBlock({ block: args.block || 'head' })));
         },
     },
 };
